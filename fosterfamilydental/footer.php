ff114210ebd0205c8d3daaf2e5bc301defeb7242
robertmeans <robert@robertmeans.com>
2015-11-17 09:16:27 -0700

    deleted fosterfamilydental dir from EWD
    don't know why it was there in the first place - ?

--- fosterfamilydental/footer.php
+++
@@ -1,5 +0,0 @@
-  	<div id="footer">
-  		<span class="spacing first">ABC Company, Inc.</span> 
-  		<span class="spacing">123 Main Street, Denver, Colorado</span> 
-  		<span class="spacing">Phone: 303.555.1212</span>
-  	</div>
