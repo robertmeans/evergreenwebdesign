f1a1a1907a4f04edfe496dfad74bde221be1db1d
robertmeans <robert@robertmeans.com>
2017-03-07 11:59:17 -0700

    added circuit board bkg to product area at top

---
+++ ecc-temp/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Evergreen Chamber issues</title>
+	<link rel="stylesheet" href="style.css?<?php echo time(); ?>">
+</head>
+<body>
+	<img class="ecc-01" src="_images/ecc-01.jpg" alt="Evergreen Chamber of Commerce">
+</body>
+</html>
