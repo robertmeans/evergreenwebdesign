0f9e96a3dd972e64dfc88d96a01a4865dc418ed5
robertmeans <robert@robertmeans.com>
2017-10-12 12:15:30 -0600

    updating new-client structure
    since using dropbox to manage all non-website related content.

--- new-client/website/index.php
+++ new-client/website/index.php
@@ -7,9 +7,10 @@
 	<meta http-equiv="expires" content="0">
 	
 	<title>ABC Company</title>
-	<!-- <link rel="icon" type="image/ico" href="_images/favicon.ico"> -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<link rel="icon" type="image/ico" href="_images/favicon.ico">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="description" content="ABC Company description here...">
+	<meta name="format-detection" content="telephone=no">
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
 	<!-- <link href='https://fonts.googleapis.com/css?family=Courgette|Lato' rel='stylesheet' type='text/css'> -->
 	<link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css">
