76977df5f102e3e6e26db0feea6496d79c9f19a1
Evergreen Web Design <info@evergreenwebdesign.com>
2018-03-10 10:09:31 -0700

    still trying to get ewd to sync

---
+++ offline-sites/wildflowerevergreen/_includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+	&copy; 2015 Wildflower Caf&eacute; | <a href="http://www.evergreenwebdesign.com" target="_blank">Evergreen Web Design</a>
+</footer>
---
+++ offline-sites/wildflowerevergreen/config.rb
@@ -0,0 +1,12 @@
+# require "susy"
+# susy is a responsive grid - using DW's instead
+css_dir = '_css'
+sass_dir = 'components/sass'
+javascripts_dir = '_scripts'
+
+#output style for compressed CSS - see Compass documentation at http://compass-style.org/help/tutorials/configuration-reference/
+
+output_style = :compressed
+# output_style = :expanded
+
+relative_assets = true;
