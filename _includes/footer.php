9b2813c1b07cb5346bdb1bbddf11fe07dc181541
Robert Means <robert@robertmeans.com>
2019-12-10 07:46:10 -0700

    Removed Chamber from footer

--- _includes/footer.php
+++ _includes/footer.php
@@ -16,7 +16,6 @@ function ewd_copyright($startYear) {
 		<p><i class="fa fa-phone-square"></i><a class="tel" tabIndex="-1" href="tel:(303)%20932-7483">(303) WEBSITE</a></p>
 		<p class="phone-spelled">That's (303) 932.7483</p>
 		<p class="copyright"><?= ewd_copyright(2015); ?> Evergreen Web Design, LLC</p>
-		<a href="http://www.evergreenchamber.org" target="_blank"><img class="ecc" src="_images/evergreenchamber-logo_02.jpg" alt="Evergreen, Colorado Chamber of Commerce"></a>
 
 	</div><!-- #footer-info -->
 </footer>
