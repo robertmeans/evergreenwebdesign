69ada1ce5c6f49b76e99c615ce910fd872dc4036
Robert Means <robert@robertmeans.com>
2024-03-14 15:33:01 -0600

    working'ish

--- components/javascript-to-compile/_scripts-staging.js
+++ components/javascript-to-compile/_scripts-staging.js
@@ -101,3 +101,38 @@ window.onmousedown = function(event) {
     modal.style.display = "none";
   }
 }
+
+/* sticky + horizontal scroll stuff begin */
+const stickySections = [...document.querySelectorAll('.sticky')]
+// console.log(stickySections);
+// let images = [
+//     '_images/can-delete/internet-vortex.jpeg',
+//     '_images/can-delete/day-sky-01.jpeg',
+//     '_images/can-delete/good-morning-late.jpeg',
+//     '_images/can-delete/night-sky.jpg',
+//     '_images/can-delete/universe-stars.jpg'
+//   ]
+
+// images.forEach(img => {
+//   stickySections.forEach(section => {
+//     let image = document.createElement('img');
+//     image.src = img;
+//     section.querySelector('.scroll-section').appendChild(image)
+//   })
+// })
+
+window.addEventListener('scroll', (e) => {
+  for (let i = 0; i < stickySections.length; i++) {
+    transform(stickySections[i])
+  }
+})
+
+function transform(section) {
+  const offsetTop = section.parentElement.offsetTop;
+  // console.log(offsetTop);
+  const scrollSection = section.querySelector('.scroll-section');
+  // console.log(scrollSection);
+  let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+  percentage = percentage < 0 ? 0 : percentage > 800 ? 800 : percentage;
+  scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`;
+}
--- index.php
+++ index.php
@@ -3,13 +3,111 @@ require_once  'config/initialize.php';
 require       '_includes/head.php';
 ?>
 <body>
-  <div class="wrap">
+<!--   <div class="wrap">
     <a class="alink epop myBtnEWD">contact</a>
     <a class="alink epop myBtnRobert">Robert</a>
     <a class="alink epop myBtnBobby">Bobby</a>
     <a class="alink epop myBtnBob">Bob</a> 
     <a class="alink epop myBtnRob">Rob</a> 
+  </div> -->
+
+
+<main>
+  
+  <section>
+    <div class="container">
+      <h1>hybrid scroll</h1>
+      <p><a href="#gohere">go here</a> Bounty loaded to the gunwalls nipperkin scallywag topsail six pounders galleon sutler Arr list. Chase Buccaneer dead men tell no tales line stern Jack Tar strike colors bilge water ye bounty. Pink scuppers keelhaul hands hornswaggle hogshead cog lee hardtack Nelsons folly.</p>
+      <p>Salmagundi jack chantey execution dock Nelsons folly tack wherry clipper reef sails Shiver me timbers. Lanyard squiffy bowsprit line transom dead men tell no tales Pieces of Eight crack Jennys tea cup shrouds heave down. Grapple cable Sink me tender pinnace Sail ho jack provost scuttle lugsail.</p>
+    </div>
+  </section>
+
+  <div class="sticky-parent">
+    <div class="sticky">
+      <div class="scroll-section">
+
+
+
+        <div class="concon">
+          <p>Jelly beans gingerbread cake liquorice apple pie candy. Jujubes lemon drops caramels topping lollipop jelly beans. Jujubes soufflé brownie tart dessert macaroon tootsie roll. Gummi bears wafer cotton candy powder muffin tootsie roll. Lollipop lollipop cake powder jelly sugar plum. Soufflé liquorice pudding jelly-o tart sesame snaps pudding soufflé.</p>
+
+          <p>Bear claw chupa chups topping icing ice cream croissant ice cream soufflé jelly-o. Macaroon jelly beans cupcake chocolate bar jelly beans ice cream. Chocolate jelly powder bear claw topping wafer. Oat cake shortbread pastry chupa chups marzipan. Lollipop topping pudding soufflé croissant shortbread. Liquorice powder candy caramels lollipop pastry halvah carrot cake chocolate bar. Cotton candy cheesecake chocolate cake chupa chups gummi bears jelly-o.</p>
+
+          <p>Gummi bears sweet roll brownie topping chupa chups marshmallow chocolate shortbread. Cheesecake liquorice donut donut cookie tiramisu. Cookie cotton candy lollipop sugar plum candy canes cotton candy. Toffee soufflé powder gummies caramels gummies chupa chups gummies. Caramels ice cream sesame snaps danish gummi bears pudding. Sugar plum bonbon caramels jujubes liquorice dessert lollipop. Powder fruitcake wafer soufflé donut chupa chups lemon drops liquorice brownie.</p>
+
+          <p>Wafer sugar plum lemon drops powder candy jelly muffin chocolate bar ice cream. Cookie cupcake dragée cake cake. Cookie candy canes topping dessert bonbon pastry danish cookie. Carrot cake wafer donut sweet danish sweet sweet. Biscuit apple pie tiramisu biscuit muffin cotton candy. Liquorice tart chupa chups liquorice shortbread. Icing cotton candy tootsie roll pastry liquorice. Cheesecake tiramisu gummies wafer oat cake tart dragée macaroon. Wafer sweet soufflé chocolate cake cheesecake gummies halvah cupcake cookie.</p>
+
+          <p>Muffin marshmallow jelly beans candy chocolate bar lemon drops. Sweet roll gummi bears cake apple pie biscuit chocolate bar. Chocolate icing jelly cookie shortbread brownie tootsie roll. Jelly beans biscuit cotton candy cotton candy oat cake halvah powder shortbread. Sugar plum jelly bonbon wafer wafer. Dragée oat cake liquorice dragée pastry. Soufflé chupa chups jelly-o bear claw tiramisu. Chocolate bar lemon drops shortbread macaroon halvah. Biscuit gummies chocolate topping halvah candy canes donut.</p>
+        </div>
+
+        <div class="concon">
+          <p>Jelly beans gingerbread cake liquorice apple pie candy. Jujubes lemon drops caramels topping lollipop jelly beans. Jujubes soufflé brownie tart dessert macaroon tootsie roll. Gummi bears wafer cotton candy powder muffin tootsie roll. Lollipop lollipop cake powder jelly sugar plum. Soufflé liquorice pudding jelly-o tart sesame snaps pudding soufflé.</p>
+
+          <p>Bear claw chupa chups topping icing ice cream croissant ice cream soufflé jelly-o. Macaroon jelly beans cupcake chocolate bar jelly beans ice cream. Chocolate jelly powder bear claw topping wafer. Oat cake shortbread pastry chupa chups marzipan. Lollipop topping pudding soufflé croissant shortbread. Liquorice powder candy caramels lollipop pastry halvah carrot cake chocolate bar. Cotton candy cheesecake chocolate cake chupa chups gummi bears jelly-o.</p>
+
+          <p>Gummi bears sweet roll brownie topping chupa chups marshmallow chocolate shortbread. Cheesecake liquorice donut donut cookie tiramisu. Cookie cotton candy lollipop sugar plum candy canes cotton candy. Toffee soufflé powder gummies caramels gummies chupa chups gummies. Caramels ice cream sesame snaps danish gummi bears pudding. Sugar plum bonbon caramels jujubes liquorice dessert lollipop. Powder fruitcake wafer soufflé donut chupa chups lemon drops liquorice brownie.</p>
+
+          <p>Wafer sugar plum lemon drops powder candy jelly muffin chocolate bar ice cream. Cookie cupcake dragée cake cake. Cookie candy canes topping dessert bonbon pastry danish cookie. Carrot cake wafer donut sweet danish sweet sweet. Biscuit apple pie tiramisu biscuit muffin cotton candy. Liquorice tart chupa chups liquorice shortbread. Icing cotton candy tootsie roll pastry liquorice. Cheesecake tiramisu gummies wafer oat cake tart dragée macaroon. Wafer sweet soufflé chocolate cake cheesecake gummies halvah cupcake cookie.</p>
+
+          <p>Muffin marshmallow jelly beans candy chocolate bar lemon drops. Sweet roll gummi bears cake apple pie biscuit chocolate bar. Chocolate icing jelly cookie shortbread brownie tootsie roll. Jelly beans biscuit cotton candy cotton candy oat cake halvah powder shortbread. Sugar plum jelly bonbon wafer wafer. Dragée oat cake liquorice dragée pastry. Soufflé chupa chups jelly-o bear claw tiramisu. Chocolate bar lemon drops shortbread macaroon halvah. Biscuit gummies chocolate topping halvah candy canes donut.</p>
+        </div>
+
+        <div id="gohere" class="concon">
+          
+          <p>Jelly beans gingerbread cake liquorice apple pie candy. Jujubes lemon drops caramels topping lollipop jelly beans. Jujubes soufflé brownie tart dessert macaroon tootsie roll. Gummi bears wafer cotton candy powder muffin tootsie roll. Lollipop lollipop cake powder jelly sugar plum. Soufflé liquorice pudding jelly-o tart sesame snaps pudding soufflé.</p>
+
+          <p>Bear claw chupa chups topping icing ice cream croissant ice cream soufflé jelly-o. Macaroon jelly beans cupcake chocolate bar jelly beans ice cream. Chocolate jelly powder bear claw topping wafer. Oat cake shortbread pastry chupa chups marzipan. Lollipop topping pudding soufflé croissant shortbread. Liquorice powder candy caramels lollipop pastry halvah carrot cake chocolate bar. Cotton candy cheesecake chocolate cake chupa chups gummi bears jelly-o.</p>
+
+          <p>Gummi bears sweet roll brownie topping chupa chups marshmallow chocolate shortbread. Cheesecake liquorice donut donut cookie tiramisu. Cookie cotton candy lollipop sugar plum candy canes cotton candy. Toffee soufflé powder gummies caramels gummies chupa chups gummies. Caramels ice cream sesame snaps danish gummi bears pudding. Sugar plum bonbon caramels jujubes liquorice dessert lollipop. Powder fruitcake wafer soufflé donut chupa chups lemon drops liquorice brownie.</p>
+
+          <p>Wafer sugar plum lemon drops powder candy jelly muffin chocolate bar ice cream. Cookie cupcake dragée cake cake. Cookie candy canes topping dessert bonbon pastry danish cookie. Carrot cake wafer donut sweet danish sweet sweet. Biscuit apple pie tiramisu biscuit muffin cotton candy. Liquorice tart chupa chups liquorice shortbread. Icing cotton candy tootsie roll pastry liquorice. Cheesecake tiramisu gummies wafer oat cake tart dragée macaroon. Wafer sweet soufflé chocolate cake cheesecake gummies halvah cupcake cookie.</p>
+
+          <p>Muffin marshmallow jelly beans candy chocolate bar lemon drops. Sweet roll gummi bears cake apple pie biscuit chocolate bar. Chocolate icing jelly cookie shortbread brownie tootsie roll. Jelly beans biscuit cotton candy cotton candy oat cake halvah powder shortbread. Sugar plum jelly bonbon wafer wafer. Dragée oat cake liquorice dragée pastry. Soufflé chupa chups jelly-o bear claw tiramisu. Chocolate bar lemon drops shortbread macaroon halvah. Biscuit gummies chocolate topping halvah candy canes donut.</p>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+        
+      </div>
+    </div>
   </div>
+
+  <section>
+    <div class="container">
+      <h1>About</h1>
+      <p>Bounty loaded to the gunwalls nipperkin scallywag topsail six pounders galleon sutler Arr list. Chase Buccaneer dead men tell no tales line stern Jack Tar strike colors bilge water ye bounty. Pink scuppers keelhaul hands hornswaggle hogshead cog lee hardtack Nelsons folly.</p>
+      <p>Salmagundi jack chantey execution dock Nelsons folly tack wherry clipper reef sails Shiver me timbers. Lanyard squiffy bowsprit line transom dead men tell no tales Pieces of Eight crack Jennys tea cup shrouds heave down. Grapple cable Sink me tender pinnace Sail ho jack provost scuttle lugsail.</p>
+    </div>
+  </section>
+
+  <div class="sticky-parent">
+    <div class="sticky">
+      <div class="scroll-section">
+
+        
+      </div>
+    </div>
+  </div>
+
+  <section>
+    <div class="container">
+      <h1>End</h1>
+      <p>Bounty loaded to the gunwalls nipperkin scallywag topsail six pounders galleon sutler Arr list. Chase Buccaneer dead men tell no tales line stern Jack Tar strike colors bilge water ye bounty. Pink scuppers keelhaul hands hornswaggle hogshead cog lee hardtack Nelsons folly.</p>
+      <p>Salmagundi jack chantey execution dock Nelsons folly tack wherry clipper reef sails Shiver me timbers. Lanyard squiffy bowsprit line transom dead men tell no tales Pieces of Eight crack Jennys tea cup shrouds heave down. Grapple cable Sink me tender pinnace Sail ho jack provost scuttle lugsail.</p>
+    </div>
+  </section>
+
+
+
+</main>
 
 
 <?php require "_includes/epop.php" ?>
